61a1eb8fc72a1b5a391ef07399c2d62ba243f7c7
Danila Glazatov <danila.glazatov123@icloud.com>
2025-04-01 20:33:46 +0300

    update button

--- pkg/app/botHandlers.go
+++ pkg/app/botHandlers.go
@@ -2,15 +2,10 @@ package app
 
 import (
 	botLib "github.com/go-telegram/bot"
-	"log"
 	"lotBot/pkg/lotBot/bot"
 )
 
 func (a *App) registerBotHandlers() {
 	a.b.RegisterHandler(botLib.HandlerTypeMessageText, "/start", botLib.MatchTypeExact, bot.StartHandler)
-}
-
-func (a *App) CallbackQueryDataHandler() {
 	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "role_", botLib.MatchTypePrefix, bot.CallbackHandler)
-	log.Println("Bot handlers registered successfully")
 }
