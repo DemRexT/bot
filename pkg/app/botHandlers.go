8a61c9ff19f42c01b557887b7fd17bd0cb6d0e71
Данила <danila.glazatov123@icloud.com>
2025-04-06 20:02:42 +0300

    implemented a dialog for a new task

--- pkg/app/botHandlers.go
+++ pkg/app/botHandlers.go
@@ -12,4 +12,8 @@ func (a *App) registerBotHandlers() {
 	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "register_", botLib.MatchTypePrefix, bot.Register)
 	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "submit_", botLib.MatchTypePrefix, bot.Moderation)
 	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "action_", botLib.MatchTypePrefix, bot.ModerationResponse)
+	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "view_tasks", botLib.MatchTypePrefix, bot.ViewTasks)
+	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "ready_", botLib.MatchTypePrefix, bot.StudentReadiness)
+	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "call", botLib.MatchTypePrefix, bot.Call)
+	a.b.RegisterHandler(botLib.HandlerTypeCallbackQueryData, "not_", botLib.MatchTypePrefix, bot.NotReady)
 }
--- pkg/lotBot/bot/bot.go
+++ pkg/lotBot/bot/bot.go
@@ -22,15 +22,36 @@ func StartHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		},
 	}
 
-	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID:      update.Message.Chat.ID,
-		Text:        "Выберите роль",
-		ReplyMarkup: kb,
-	})
-	if err != nil {
-		fmt.Println(fmt.Errorf("%v", err))
-		return
+	if update.CallbackQuery != nil && update.CallbackQuery.Data == "start" {
+		_, err := b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+			CallbackQueryID: update.CallbackQuery.ID,
+			ShowAlert:       false,
+		})
+		if err != nil {
+			return
+		}
+
+		// Вызываем ту же логику, что и для /start
+		_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:      update.CallbackQuery.Message.Message.Chat.ID,
+			Text:        "Выберите роль",
+			ReplyMarkup: kb,
+		})
+		if err != nil {
+			return
+		}
+	} else {
+		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:      update.Message.Chat.ID,
+			Text:        "Выберите роль",
+			ReplyMarkup: kb,
+		})
+		if err != nil {
+			fmt.Println(fmt.Errorf("%v", err))
+			return
+		}
 	}
+
 }
 
 func CallbackHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
@@ -254,4 +275,181 @@ func ModerationResponse(ctx context.Context, b *bot.Bot, update *models.Update) 
 		log.Printf("Ошибка отправки сообщения: %v", err)
 	}
 
+}
+
+func ViewTasks(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: update.CallbackQuery.ID,
+		ShowAlert:       false,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+	kb := &models.InlineKeyboardMarkup{
+		InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: "Готов", CallbackData: "ready_yes"},
+				{Text: "Не готов", CallbackData: "ready_not"},
+			},
+		},
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.CallbackQuery.Message.Message.Chat.ID,
+		Text: "У нас есть для тебя задание!\nПожалуйста. ознакомься с заданием.\n" +
+			"Срок для изучения задания - до ЧЧ.ММ ДД.ММ\n" +
+			"Пришлем напоминалку поле этого срока и уточним готовность.\n" +
+			"И помни: мы не выполним задание за тебя,\nно обязательно поможем и подскажем,\n" +
+			"если будет трудно или непонятно!",
+		ReplyMarkup: kb,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+}
+
+func StudentReadiness(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: update.CallbackQuery.ID,
+		ShowAlert:       false,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	var response string
+	var kb *models.InlineKeyboardMarkup
+	switch update.CallbackQuery.Data {
+	case "ready_yes":
+		response = "Отлично!\nДавай назначим созвон с заказчиком длявыяснения деталей,затем ты сможеш приступить"
+		kb = &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "Окей", CallbackData: "call"},
+				},
+			},
+		}
+	case "ready_not":
+		response = "Подскажи, почему именно:"
+		kb = &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "еще занят с предыдущей задачей", CallbackData: "not_busy"},
+				},
+				{
+					{Text: "задача мне не интересна", CallbackData: "not_interesting"},
+				},
+				{
+					{Text: "не понял задание и/или не уверен, что справлюсь", CallbackData: "not_understand"},
+				},
+			},
+		}
+	default:
+		response = "Неизвестная команда: " + update.CallbackQuery.Data
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      update.CallbackQuery.Message.Message.Chat.ID,
+		Text:        response,
+		ReplyMarkup: kb,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+}
+
+func Call(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: update.CallbackQuery.ID,
+		ShowAlert:       false,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	kb := &models.InlineKeyboardMarkup{
+		InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: "Готово", CallbackData: "_"},
+			},
+		},
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      update.CallbackQuery.Message.Message.Chat.ID,
+		Text:        "Отлично! Давай назначим созвон",
+		ReplyMarkup: kb,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: AdminChatID,
+		Text:   "Запрос на новый созвон от пользователя!",
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+}
+
+func NotReady(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: update.CallbackQuery.ID,
+		ShowAlert:       false,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	parts := strings.Split(update.CallbackQuery.Data, "_")
+	var kb *models.InlineKeyboardMarkup
+	var response string
+	switch parts[1] {
+	case "busy":
+		response = "Хочешь взять это задание следующим после текущего?"
+		kb = &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "Да", CallbackData: "call_you"},
+					{Text: "Нет", CallbackData: "following_tasks"},
+				},
+			},
+		}
+	case "interesting":
+		response = "Больше не отправлять тебе задачи из этого трека?"
+		kb = &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "Да, не моё направление", CallbackData: "_"},
+					{Text: "Нет, отправляйте другие, не зашло именно это", CallbackData: "_"},
+				},
+			},
+		}
+	case "understand":
+		response = "Хочешь задать вопросы и получить \\более подробное пояснение?"
+		kb = &models.InlineKeyboardMarkup{
+			InlineKeyboard: [][]models.InlineKeyboardButton{
+				{
+					{Text: "да, пожалуйста  (связь в личке или созвон)", CallbackData: "call_you"},
+					{Text: "нет, спасибо", CallbackData: "following_tasks"},
+				},
+			},
+		}
+	default:
+		response = "Неизвестная команда: " + update.CallbackQuery.Data
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      update.CallbackQuery.Message.Message.Chat.ID,
+		Text:        response,
+		ReplyMarkup: kb,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+		log.Printf("%v", update.CallbackQuery.Data)
+	}
 }
