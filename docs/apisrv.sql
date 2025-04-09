534f0005105840be1206f74b574220a83b20fe49
Ivan <munasef.vafl@gmail.com>
2025-04-09 18:37:08 +0300

    Add files via upload

--- docs/apisrv.sql
+++ docs/apisrv.sql
@@ -1,131 +1,60 @@
 ﻿-- =============================================================================
 -- Diagram Name: apisrv
--- Created on: 6/8/2022 12:02:23 PM
+-- Created on: 06.04.2025 18:02:07
 -- Diagram Version: 
 -- =============================================================================
 
-CREATE TABLE "statuses" (
-	"statusId" SERIAL NOT NULL,
-	"title" varchar(255) NOT NULL,
-	"alias" varchar(64) NOT NULL,
-	CONSTRAINT "statuses_pkey" PRIMARY KEY("statusId"),
-	CONSTRAINT "statuses_alias_key" UNIQUE("alias")
-);
-
-CREATE TABLE "users" (
-	"userId" SERIAL NOT NULL,
-	"login" varchar(64) NOT NULL,
-	"password" varchar(64) NOT NULL,
-	"authKey" varchar(32),
-	"createdAt" timestamp with time zone NOT NULL DEFAULT now(),
-	"lastActivityAt" timestamp with time zone,
+CREATE TABLE "companies" (
+	"companyId" SERIAL NOT NULL,
+	"name" text NOT NULL,
+	"tgId" text NOT NULL,
+	"inn" int4 NOT NULL,
+	"scope" text NOT NULL,
+	"userName" name NOT NULL,
+	"phone" int4 NOT NULL,
 	"statusId" int4 NOT NULL,
-	CONSTRAINT "users_pkey" PRIMARY KEY("userId")
+	PRIMARY KEY("companyId")
 );
 
-CREATE INDEX "IX_FK_users_statusId_users" ON "users" USING BTREE (
-	"statusId"
-);
-
-
-CREATE TABLE "vfsFiles" (
-	"fileId" SERIAL NOT NULL,
-	"folderId" int4 NOT NULL,
-	"title" varchar(255) NOT NULL,
-	"path" varchar(255) NOT NULL,
-	"params" text,
-	"isFavorite" bool DEFAULT false,
-	"mimeType" varchar(255) NOT NULL,
-	"fileSize" int4 DEFAULT 0,
-	"fileExists" bool NOT NULL DEFAULT true,
-	"createdAt" timestamp NOT NULL DEFAULT now(),
+CREATE TABLE "tasks" (
+	"taskId" SERIAL NOT NULL,
+	"companyId" int4 NOT NULL,
+	"scope" text NOT NULL,
+	"description" text NOT NULL,
+	"images" varchar(128)[] NOT NULL,
+	"link" text NOT NULL,
+	"deadline" date NOT NULL,
+	"contactSlot text" text NOT NULL,
 	"statusId" int4 NOT NULL,
-	CONSTRAINT "vfsFiles_pkey" PRIMARY KEY("fileId")
+	"studentId" int4,
+	PRIMARY KEY("taskId")
 );
 
-CREATE INDEX "IX_FK_vfsFiles_folderId_vfsFiles" ON "vfsFiles" USING BTREE (
-	"folderId"
-);
-
-
-CREATE INDEX "IX_FK_vfsFiles_statusId_vfsFiles" ON "vfsFiles" USING BTREE (
-	"statusId"
-);
-
-
-CREATE TABLE "vfsFolders" (
-	"folderId" SERIAL NOT NULL,
-	"parentFolderId" int4,
-	"title" varchar(255) NOT NULL,
-	"isFavorite" bool DEFAULT false,
-	"createdAt" timestamp NOT NULL DEFAULT now(),
+CREATE TABLE "students" (
+	"studentId" SERIAL NOT NULL,
+	"tgId" text NOT NULL,
+	"name" text NOT NULL,
+	"birthday" date NOT NULL,
+	"city" text NOT NULL,
+	"scope" text NOT NULL,
+	"email" text NOT NULL,
 	"statusId" int4 NOT NULL,
-	CONSTRAINT "vfsFolders_pkey" PRIMARY KEY("folderId")
-);
-
-CREATE INDEX "IX_FK_vfsFolders_folderId_vfsFolders" ON "vfsFolders" USING BTREE (
-	"parentFolderId"
+	PRIMARY KEY("studentId")
 );
 
 
-CREATE INDEX "IX_FK_vfsFolders_statusId_vfsFolders" ON "vfsFolders" USING BTREE (
-	"statusId"
-);
-
-
-CREATE TABLE "vfsHashes" (
-	"hash" varchar(40) NOT NULL,
-	"namespace" varchar(32) NOT NULL,
-	"extension" varchar(4) NOT NULL,
-	"fileSize" int4 NOT NULL DEFAULT 0,
-	"width" int4 NOT NULL DEFAULT 0,
-	"height" int4 NOT NULL DEFAULT 0,
-	"blurhash" text,
-	"error" text,
-	"createdAt" timestamp with time zone NOT NULL DEFAULT now(),
-	"indexedAt" timestamp with time zone,
-	CONSTRAINT "vfsHashes_pkey" PRIMARY KEY("hash","namespace")
-);
-
-CREATE INDEX "IX_vfsHashes_indexedAt" ON "vfsHashes" USING BTREE (
-	"indexedAt"
-);
-
-
-
-ALTER TABLE "users" ADD CONSTRAINT "FK_users_statusId" FOREIGN KEY ("statusId")
-	REFERENCES "statuses"("statusId")
+ALTER TABLE "tasks" ADD CONSTRAINT "Ref_tasks_to_companies" FOREIGN KEY ("companyId")
+	REFERENCES "companies"("companyId")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
-ALTER TABLE "vfsFiles" ADD CONSTRAINT "vfsFiles_folderId_fkey" FOREIGN KEY ("folderId")
-	REFERENCES "vfsFolders"("folderId")
+ALTER TABLE "tasks" ADD CONSTRAINT "Ref_tasks_to_students" FOREIGN KEY ("studentId")
+	REFERENCES "students"("studentId")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
-	NOT DEFERRABLE;
-
-ALTER TABLE "vfsFiles" ADD CONSTRAINT "vfsFiles_statusId_fkey" FOREIGN KEY ("statusId")
-	REFERENCES "statuses"("statusId")
-	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
-	NOT DEFERRABLE;
-
-ALTER TABLE "vfsFolders" ADD CONSTRAINT "vfsFolders_parentFolderId_fkey" FOREIGN KEY ("parentFolderId")
-	REFERENCES "vfsFolders"("folderId")
-	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
-	NOT DEFERRABLE;
-
-ALTER TABLE "vfsFolders" ADD CONSTRAINT "vfsFolders_statusId_fkey" FOREIGN KEY ("statusId")
-	REFERENCES "statuses"("statusId")
-	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
 
