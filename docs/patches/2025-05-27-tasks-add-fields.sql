9e4086dfcaf035ff75d1cd91f4c654e030b1b875
Данила <danila.glazatov123@icloud.com>
2025-05-28 12:18:43 +0300

    edits

--- docs/patches/2025-05-27-tasks-add-fields.sql
+++ docs/patches/2025-05-27-tasks-add-fields.sql
@@ -1,11 +1,17 @@
-alter table tasks
+alter table public.companies
+    add "createdAt" timestamp default now() not null;
+
+alter table public.tasks
     add name text;
 
-alter table tasks
-alter column deadline type text using deadline::text;
-
-alter table tasks
+alter table public.tasks
     add url text;
 
-alter table tasks
+alter table public.tasks
     add "createdAt" timestamp default now() not null;
+
+alter table public.students
+    add "createdAt" timestamp default now() not null;
+
+alter table public.tasks
+alter column deadline type text using deadline::text;
